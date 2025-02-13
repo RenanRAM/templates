ef55910071cffd29c771801412e4ad5f458f3f96
Renan_RAM <renanmagico22@gmail.com>
2025-02-13 03:59:09 -0300

    Melhorias em andamento

--- js/mascara/numascara.js
+++ js/mascara/numascara.js
@@ -1,6 +1,125 @@
 
-document.addEventListener('DOMContentLoaded', carregado);
+//TODO: fazer método para acrescentar ou setar os aliase
 
+class initnm{
+	static #gabaritosMascaras = {};
+	static #aliases = {mascara2 : '5-4',
+		fone:'(2) 5-4'};
+
+	static iniciar(){
+
+		initnm.#forSimpleInputs((ele,index)=>{
+			const chave = 'mascara_identificador'+index;
+			const mascaraValor = ele.getAttribute("numascara");
+			if(mascaraValor === '') return;
+			//fazer os gabaritos
+			initnm.#gabaritosMascaras[chave] = initnm.#interpretarMask(mascaraValor);
+			ele.addEventListener('input',()=>{
+				//mudar o valor do input interpretando a mascara
+				const textoInicial = ele.value;
+				ele.value = initnm.#aplicarMask(initnm.#gabaritosMascaras[chave],textoInicial);
+			});	
+		});
+
+
+		initnm.#forAliasInputs(ele=>{
+			const mascaraNome = ele.getAttribute("mascara");
+			if(mascaraNome === '') return;
+			const masc = initnm.#aliases[mascaraNome];
+			if(masc === '') return;
+			//fazer os gabaritos
+			initnm.#gabaritosMascaras[mascaraNome] = initnm.#interpretarMask(masc);
+			ele.addEventListener('input',()=>{
+				//mudar o valor do input interpretando a mascara
+				const textoInicial = ele.value;
+				ele.value = initnm.#aplicarMask(initnm.#gabaritosMascaras[mascaraNome],textoInicial);
+			});
+		});
+	}
+
+	static #forSimpleInputs(fn){
+		const inputs = document.querySelectorAll('input[numascara]');
+		inputs.forEach(fn);
+	}
+
+	static #forAliasInputs(fn){
+		const inputs = document.querySelectorAll('input[mascara]');
+		inputs.forEach(fn);
+	}
+
+	static #aplicarMask(gabarito,texto_pre){//funcionando
+		if(!(Array.isArray(gabarito))){
+			return false;
+		}
+		let texto = texto_pre.replace(/\D/g, '');//remover não números do texto aqui
+		if(texto == ''){
+			return '';
+		}
+		const tam_txt = texto.length;
+		const tam_gab = gabarito.length;
+		let aplicado = '';
+		let i_gab = 0;
+		let i_txt = 0;
+		let aux = 0;
+		principal:
+		for(i_gab = 0; i_gab < tam_gab;i_gab++){//percorrer gabarito
+			if(!(typeof gabarito[i_gab] === 'number')){
+				aplicado += gabarito[i_gab];
+			}else{
+				aux = 0;
+				for(;i_txt<tam_txt;i_txt++){
+					if(aux == gabarito[i_gab]){//verificar se já foi tudo preenchido
+						break;
+					}
+					aplicado+=texto.charAt(i_txt);
+					aux++;
+				}
+				if(i_txt==tam_txt){// o texto acabou, sair de tudo
+					break principal;
+				}
+			}
+		}
+		return aplicado;
+	}
+
+	static #interpretarMask(mask){//funcionando
+		if(typeof mask !== "string") return false;
+			const tam = mask.length;
+			let vetor = [];
+			let aux = '';
+			let char = null;
+			let char_aux = null;
+			let fim = false;
+			for(let i = 0;i<tam;i++){
+				char = mask.charAt(i);
+				if(isNaN(Number(char)) || (char===' ')){//verifica se o caracter não é um número ou é ' '
+					//não é um número
+					vetor.push(char);
+				}else{
+					//é um número
+					aux = '';
+					char_aux = null;
+					fim = true;//se o laço 'for' acabar naturalmente quer dizer que acabou a string da mascara
+					for(let a = i;a<tam;a++){
+						char_aux = mask.charAt(a);
+						if(isNaN(Number(char_aux)) || (char_aux===' ')){
+							i = --a;//decrementar 1, pois o atual não é número
+							fim = false;//ainda não acabou a string mask
+							break;
+						}
+						aux += char_aux;
+					}
+					vetor.push(Number(aux));
+					if(fim){
+						break;
+					}
+				}
+			}
+		return vetor;
+	}
+}
+
+/*
 function interpretarMask(mask){//funcionando
 	if(typeof mask === "string"){
 		const tam = mask.length;
@@ -111,3 +230,5 @@ function carregado(){
 	});
 	return false;
 }
+*/
+document.addEventListener('DOMContentLoaded', initnm.iniciar);
